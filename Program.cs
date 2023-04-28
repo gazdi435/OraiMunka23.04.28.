409e8718661358d08bc039133abfda2c4ca4d3d9
Zsolesz <72067999+gazdi435@users.noreply.github.com>
2023-04-28 13:17:40 +0200

    Add files via upload
    https://classroom.google.com/c/NTQ2MTIwNTU1NjE0/m/NjA2NzU1NDQ4MTE2/details

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace SQLLekerdezesGyakorlas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string kapcsolatLeiro = "datasource=127.0.0.1;port=3306;database=hardver;username=root;password=;Charset=utf8;";
+            MySqlConnection SQLkapcsolat = new MySqlConnection(kapcsolatLeiro);
+
+            try
+            {
+                SQLkapcsolat.Open();
+            }
+            catch (MySqlException)
+            {
+                Console.WriteLine("Nem lehet csatlakozni az adatbázishoz!");
+                Environment.Exit(1);
+            }
+
+            string SQLLekerdezes = "SELECT DISTINCT Gyártó FROM termékek group by Gyártó having COUNT(Kategória = 'Monitor') > 39;";
+
+            MySqlCommand SQLparancs = new MySqlCommand(SQLLekerdezes, SQLkapcsolat);
+
+            MySqlDataReader eredmenyolvaso = SQLparancs.ExecuteReader();
+
+            while (eredmenyolvaso.Read())
+            {
+                Console.WriteLine(eredmenyolvaso.GetString("Gyártó"));
+            }
+
+            eredmenyolvaso.Close();
+
+            SQLkapcsolat.Close();
+        }
+    }
+}
---
+++ feladat2.cs
@@ -0,0 +1,40 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace SQLLekerdezesGyakorlas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string kapcsolatLeiro = "datasource=127.0.0.1;port=3306;database=hardver;username=root;password=;charset=utf8";
+
+            MySqlConnection SQLkapcsolat = new MySqlConnection(kapcsolatLeiro);
+
+            try
+            {
+                SQLkapcsolat.Open();
+            }
+            catch (MySqlException)
+            {
+
+                Console.WriteLine("Nem lehet csatlakozni az adatbázishoz!");
+                Environment.Exit(1);
+            }
+
+            string strLekerdezes = "SELECT  DISTINCT * FROM termékek WHERE Ár > (SELECT ár FROM termékek WHERE Ár = (SELECT MAX(Ár) FROM termékek WHERE Kategória LIKE 'winchester%' AND Gyártó LIKE 'seagate%')) AND Kategória LIKE 'winchester%' ORDER BY termékek.Ár ASC;";
+
+            MySqlCommand sqlParancs= new MySqlCommand(strLekerdezes, SQLkapcsolat);
+
+            MySqlDataReader sqlEredmenyolvaso = sqlParancs.ExecuteReader();
+
+            while (sqlEredmenyolvaso.Read())
+            {
+                Console.WriteLine($"{sqlEredmenyolvaso.GetString("Cikkszám")} | {sqlEredmenyolvaso.GetString("Kategória")} | {sqlEredmenyolvaso.GetString("Gyártó")} | {sqlEredmenyolvaso.GetString("Név")} | {sqlEredmenyolvaso.GetString("Ár")} | {sqlEredmenyolvaso.GetString("Garidő")} | {sqlEredmenyolvaso.GetString("Készlet")} | {sqlEredmenyolvaso.GetString("Súly")}");
+            }
+
+            sqlEredmenyolvaso.Close();
+            SQLkapcsolat.Close();
+
+        }
+    }
+}
